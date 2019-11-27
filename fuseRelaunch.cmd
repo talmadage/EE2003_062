f61a4352189b9bd0ef4b33089745d0c2ffe10c42
talmadage <40017260+talmadage@users.noreply.github.com>
2019-11-27 16:04:13 +0530

    Add files via upload

---
+++ fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "X:/ass5/cpu_tb_isim_beh.exe" -prj "X:/ass5/cpu_tb_beh.prj" "work.cpu_tb" "work.glbl" 
---
+++ ucf.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:31:23 11/27/2019 
+// Design Name: 
+// Module Name:    ucf 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ucf(
+    );
+
+
+endmodule
